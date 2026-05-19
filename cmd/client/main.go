1b7c47a0ae18d0c603c786cbad5e6d113ae3fad2
Vaivaswat <vaivaswat2244@gmail.com>
2026-05-20 00:04:10 +0530

    fix: fixed entry for magnet links

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -6,28 +6,74 @@ import (
 	"log"
 	"time"
 
+	"github.com/Vaivaswat2244/go-torrent/internal/bencode"
+	"github.com/Vaivaswat2244/go-torrent/internal/dht"
 	"github.com/Vaivaswat2244/go-torrent/internal/engine"
+	"github.com/Vaivaswat2244/go-torrent/internal/magnet"
+	"github.com/Vaivaswat2244/go-torrent/internal/metadata"
 	"github.com/Vaivaswat2244/go-torrent/internal/torrentfile"
 )
 
 func main() {
 	torrentPath := flag.String("torrent", "", "Path to .torrent file")
+	magnetLink := flag.String("magnet", "", "Magnet link string")
 	outputDir := flag.String("output", ".", "Output directory")
 	flag.Parse()
 
-	if *torrentPath == "" {
-		log.Fatal("Usage: torrent -torrent <path> [-output <dir>]")
+	if *torrentPath == "" && *magnetLink == "" {
+		log.Fatal("Usage: torrent [-torrent <path> | -magnet <link>] [-output <dir>]")
 	}
-
 	var peerID [20]byte
 	copy(peerID[:], "-GO0001-123456789012")
 
-	tf, err := torrentfile.Open(*torrentPath)
-	if err != nil {
-		log.Fatalf("Failed to parse torrent file: %v", err)
-	}
+	var tf *torrentfile.TorrentFile
+	var err error
 
-	fmt.Printf("📁 Loaded: %s (%.2f MB)\n", tf.Name, float64(tf.Length)/1024/1024)
+	if *magnetLink != "" {
+		fmt.Println("🧲 Parsing Magnet Link...")
+		mag, err := magnet.Parse(*magnetLink)
+		if err != nil {
+			log.Fatalf("Failed to parse magnet link: %v", err)
+		}
+		fmt.Printf("InfoHash: %x\n", mag.InfoHash)
+
+		// 1. Create a channel for peers
+		peerChan := make(chan torrentfile.Peer, 100)
+
+		// 2. Start the DHT Crawler in the background
+		go dht.FindPeers(mag.InfoHash, peerChan)
+
+		// 3. Pause and wait for the Metadata Fetcher!
+		// THIS replaces the "stub" logic. It downloads the real piece hashes over TCP!
+		rawInfo, err := metadata.Fetch(mag.InfoHash, peerID, peerChan)
+		if err != nil {
+			log.Fatalf("Fatal: Could not fetch metadata from network: %v", err)
+		}
+
+		// 4. Reconstruct the TorrentFile using our bencode decoder
+		infoDictVal, err := bencode.Decode(rawInfo)
+		if err != nil {
+			log.Fatalf("Failed to decode downloaded metadata: %v", err)
+		}
+
+		infoDict := infoDictVal.(map[string]bencode.Value)
+
+		// 5. Parse the dictionary into our TorrentFile struct
+		tf, err = torrentfile.ParseInfoDict(infoDict, mag.InfoHash)
+		if err != nil {
+			log.Fatalf("Failed to map downloaded metadata: %v", err)
+		}
+
+		tf.Name = mag.Name
+		fmt.Printf("📁 Metadata Downloaded! Size: %.2f MB\n", float64(tf.Length)/1024/1024)
+
+	} else {
+		tf, err = torrentfile.Open(*torrentPath)
+		if err != nil {
+			log.Fatalf("Failed to parse torrent file: %v", err)
+		}
+		fmt.Printf("📁 Loaded: %s (%.2f MB)\n", tf.Name, float64(tf.Length)/1024/1024)
+	}
 
 	torrent, err := engine.NewTorrent(tf, *outputDir)
 	if err != nil {
@@ -56,5 +102,5 @@ func main() {
 		time.Sleep(1 * time.Second)
 	}
 
-	fmt.Println("Daemon shutting down.")
+	fmt.Println("\nDaemon shutting down.")
 }
